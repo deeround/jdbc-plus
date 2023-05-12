8fcfcfbe63a2cc67f96c4743469d040dcf83727b
deeround <913351190@qq.com>
2023-05-12 09:36:45 +0800

    -

--- jdbc-plus-samples/src/test/java/com/github/deeround/jdbc/plus/samples/Tests.java
+++ jdbc-plus-samples/src/test/java/com/github/deeround/jdbc/plus/samples/Tests.java
@@ -43,17 +43,6 @@ public class Tests {
         System.out.println(page3);
     }
 
-    /**
-     * 分页查询：jdbc-plus和mybatis-plus查询使用对比
-     */
-    @Test
-    void testPage() {
-        PageInfo<Map<String, Object>> page1 = this.jdbcTemplateTestService.page1();
-        Page<TestUser> page2 = this.testUserService.page(new Page<TestUser>(1, 2));
-        log.info("total:{},records:{},page1:{}", page1.getTotal(), page1.getList().size(), page1.getList());
-        log.info("total:{},records:{},page2:{}", page2.getTotal(), page2.getRecords().size(), page2.getRecords());
-    }
-
     @Test
     void testTenantInsert() {
         this.jdbcTemplateTestService.insert();
@@ -69,15 +58,10 @@ public class Tests {
         this.jdbcTemplateTestService.delete();
     }
 
-    /**
-     * 普通查询：jdbc-plus和mybatis-plus查询使用对比
-     */
     @Test
     void testTenantQuery() {
         List<Map<String, Object>> list1 = this.jdbcTemplateTestService.query();
-        List<TestUser> list2 = this.testUserService.list();
         System.out.println(list1);
-        System.out.println(list2);
     }
 
     @Test
@@ -91,16 +75,42 @@ public class Tests {
     }
 
 
+    //======================jdbc-plus和mybatis-plus使用对比==============================
+
+    /**
+     * 普通查询：jdbc-plus和mybatis-plus查询使用对比
+     */
+    @Test
+    void testQueryWithMp() {
+        List<Map<String, Object>> list1 = this.jdbcTemplateTestService.query();
+        List<TestUser> list2 = this.testUserService.list();
+        System.out.println(list1);
+        System.out.println(list2);
+    }
+
+    /**
+     * 分页查询：jdbc-plus和mybatis-plus查询使用对比
+     */
+    @Test
+    void testPageWithMp() {
+        PageInfo<Map<String, Object>> page1 = this.jdbcTemplateTestService.page1();
+        Page<TestUser> page2 = this.testUserService.page(new Page<TestUser>(1, 2));
+        log.info("total:{},records:{},page1:{}", page1.getTotal(), page1.getList().size(), page1.getList());
+        log.info("total:{},records:{},page2:{}", page2.getTotal(), page2.getRecords().size(), page2.getRecords());
+    }
+
     /**
      * 条件查询：jdbc-plus和mybatis-plus查询使用对比
      * xml中编写条件查询语句
      */
     @Test
-    void testQueryByCondition() {
+    void testQueryByConditionWithMp() {
         List<Map<String, Object>> list1 = this.jdbcTemplateTestService.getListByCondition("3");
         List<TestUser> list2 = this.testUserService.getListByCondition("3");
         System.out.println(list1);
         System.out.println(list2);
     }
+
+    //======================jdbc-plus和mybatis-plus使用对比==============================
 
 }
